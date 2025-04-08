3429abe3f1f2118616988451395c36d5eed830d0
OwnerOfJK <john.kaller@hotmail.de>
2025-04-08 15:31:14 +0100

    Run prettier

--- app.config.js
+++ app.config.js
@@ -2,41 +2,38 @@ const IS_DEV = process.env.APP_VARIANT === 'development';
 const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
 
 const getUniqueIdentifier = () => {
-    if (IS_DEV) {
-      return 'com.jhk.linda.dev';
-    }
-  
-    if (IS_PREVIEW) {
-      return 'com.jhk.linda.preview';
-    }
-  
-    return 'com.jhk.linda';
-  };
-  
-  const getAppName = () => {
-    if (IS_DEV) {
-      return 'Linda (Dev)';
-    }
-  
-    if (IS_PREVIEW) {
-      return 'Linda (Preview)';
-    }
-  
-    return 'Linda: Find your friends';
-  };
-  
+  if (IS_DEV) {
+    return 'com.jhk.linda.dev';
+  }
 
-  export default ({ config }) => ({
-    ...config,
-    name: getAppName(),
-    ios: {
-      ...config.ios,
-      bundleIdentifier: getUniqueIdentifier(),
-    },
-    android: {
-      ...config.android,
-      package: getUniqueIdentifier(),
-    },
-  });
-  
-  
+  if (IS_PREVIEW) {
+    return 'com.jhk.linda.preview';
+  }
+
+  return 'com.jhk.linda';
+};
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return 'Linda (Dev)';
+  }
+
+  if (IS_PREVIEW) {
+    return 'Linda (Preview)';
+  }
+
+  return 'Linda: Find your friends';
+};
+
+export default ({ config }) => ({
+  ...config,
+  name: getAppName(),
+  ios: {
+    ...config.ios,
+    bundleIdentifier: getUniqueIdentifier(),
+  },
+  android: {
+    ...config.android,
+    package: getUniqueIdentifier(),
+  },
+});
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,7 @@
 import './global.css';
 import * as Sentry from '@sentry/react-native';
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import React from 'react';
 
 Sentry.init({
   dsn: 'https://7bd189bad5d479ab9e6b74c6d8cc0647@o4509108953939968.ingest.de.sentry.io/4509108958789712',
@@ -10,7 +11,5 @@ Sentry.init({
 });
 
 export default Sentry.wrap(function RootLayout() {
-  return (
-    <Stack />
-  );
+  return <Stack />;
 });
