c5cdfe3972e70d5903de8a8e481d2ca3b9302e73
OwnerOfJK <john.kaller@hotmail.de>
2025-04-08 21:07:14 +0100

    Remove app.json && update app.config.js

--- app.config.js
+++ app.config.js
@@ -2,38 +2,66 @@ const IS_DEV = process.env.APP_VARIANT === 'development';
 const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
 
 const getUniqueIdentifier = () => {
-  if (IS_DEV) {
-    return 'com.jhk.linda.dev';
-  }
-
-  if (IS_PREVIEW) {
-    return 'com.jhk.linda.preview';
-  }
-
+  if (IS_DEV) return 'com.jhk.linda.dev';
+  if (IS_PREVIEW) return 'com.jhk.linda.preview';
   return 'com.jhk.linda';
 };
 
 const getAppName = () => {
-  if (IS_DEV) {
-    return 'Linda (Dev)';
-  }
-
-  if (IS_PREVIEW) {
-    return 'Linda (Preview)';
-  }
-
+  if (IS_DEV) return 'Linda (Dev)';
+  if (IS_PREVIEW) return 'Linda (Preview)';
   return 'Linda: Connected, Globally';
 };
 
 export default ({ config }) => ({
   ...config,
   name: getAppName(),
+  slug: 'linda',
+  owner: 'jhk',
   ios: {
     ...config.ios,
     bundleIdentifier: getUniqueIdentifier(),
+    supportsTablet: true,
+    infoPlist: {
+      ITSAppUsesNonExemptEncryption: false,
+    },
   },
   android: {
     ...config.android,
     package: getUniqueIdentifier(),
+    adaptiveIcon: {
+      foregroundImage: 'src/assets/adaptive-icon.png',
+      backgroundColor: '#ffffff',
+    },
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+    url: 'https://u.expo.dev/37f14e48-435e-424c-88b1-49d98296941e',
+  },
+  extra: {
+    eas: {
+      projectId: '37f14e48-435e-424c-88b1-49d98296941e',
+    },
+  },
+  expo: {
+    userInterfaceStyle: 'automatic',
+  },
+  plugins: [
+    [
+      '@sentry/react-native/expo',
+      {
+        url: 'https://sentry.io/',
+        project: 'react-native',
+        organization: 'linda-0a',
+      },
+    ],
+    ['expo-router'],
+  ],
+  assetBundlePatterns: ['**/*'],
+  orientation: 'portrait',
+  newArchEnabled: true,
+  icon: 'src/assets/icon.png',
+  web: {
+    favicon: 'src/assets/favicon.png',
   },
 });
