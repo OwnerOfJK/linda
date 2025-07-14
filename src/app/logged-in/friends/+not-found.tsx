c72d86f8c7abaad0072c9014d9adccf40c275eb0
OwnerOfJK <john.kaller@hotmail.de>
2025-07-14 20:46:30 +0200

    Move not found page

--- src/app/logged-in/friends/+not-found.tsx
+++ src/app/logged-in/friends/+not-found.tsx
@@ -3,11 +3,11 @@ import { View, Text } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 
 export default function NotFound() {
-  const { userName } = useLocalSearchParams();
+  const { friendId } = useLocalSearchParams();
 
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>{userName} Not Found</Text>
+      <Text>{friendId} Not Found</Text>
     </View>
   );
 }
