600f9c7fe301f9003dbce41b427b3048ee51eaa0
OwnerOfJK <john.kaller@hotmail.de>
2025-07-14 20:45:37 +0200

    Remove landing page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,13 @@
+import { Text, View, Button } from 'react-native';
+import { useRouter } from 'expo-router';
 import React from 'react';
-import LandingPage from './landing';
 
 export default function Index() {
-  return <LandingPage />;
+  const router = useRouter();
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="mb-4 text-2xl font-bold">Welcome to Linda</Text>
+      <Button title="Login here" onPress={() => router.navigate('/login/login')} />
+    </View>
+  );
 }
